74e3ec9ccb7a61ebb9a2330324c543553b0809f4
Adeputras17 <kirabelkparg@gmail.com>
2023-02-08 10:34:44 +0800

    update

--- Nomor1.js
+++ Nomor1.js
@@ -1,16 +1,12 @@
-function faktorial (int) {
-    if (int === 4){
-        return 24;
-    } else if (int === 8){
-        return 4032;
-    } else {
-        let hasil = 1;
-        for (let i = 1; i <= int; i++){
-            hasil *= i;
-        }
-        return hasil;
-    } 
+function faktorial(int) {
+    let hasil = 1;
+    for (let i = 1; i <= int; i++) {
+        hasil *= i;
+    }
+    return hasil;
+
 }
 
 console.log(faktorial(4));
 console.log(faktorial(8));
+console.log(faktorial(5));
